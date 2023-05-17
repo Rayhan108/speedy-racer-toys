144f420333c89f1243d18be3dee31b3fa471ee61
Rayhan Shorker <raihanshorker56@gmail.com>
2023-05-18 00:40:25 +0600

    navbar added

--- src/layout/Main/MainLayout.jsx
+++ src/layout/Main/MainLayout.jsx
@@ -1,6 +1,7 @@
 import { Outlet} from 'react-router-dom';
-import Navbar from '../../pages/Home/Navbar/Navbar';
-import Footer from '../../pages/Home/Footer/Footer';
+import Navbar from '../../pages/Home/shared/Navbar/Navbar';
+import Footer from '../../pages/Home/shared/Footer/Footer';
+
 
 const MainLayout = () => {
     return (
---
+++ src/pages/Home/shared/Navbar/Navbar.jsx
@@ -0,0 +1,74 @@
+import { Link } from "react-router-dom";
+import logo from '../../../../assets/logo.png'
+import './Navbar.css'
+const Navbar = () => {
+  const navItems = (
+    <>
+      <li>
+        <Link className=" font-bold" to="/">Home</Link>
+      </li>
+      <li>
+        <Link className=" font-bold">All Toys</Link>
+      </li>
+      <li>
+        {" "}
+        <Link className=" font-bold">My Toys</Link>
+      </li>
+      <li>
+        <Link className=" font-bold">Add Toys</Link>
+        <Link className=" font-bold">Blogs</Link>
+      </li>
+    </>
+  );
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            
+           {navItems}
+          </ul>
+        </div>
+     
+            <img className="circle" src={logo} style={{width:"120px"}}/>
+       
+        <Link className=" normal-case text-2xl font-bold">Speedy-Racer-Toys</Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+        {navItems}
+        </ul>
+      </div>
+    
+      <div className="navbar-end">
+      <label tabIndex={0} className="btn btn-ghost btn-circle avatar mr-5">
+          <div className="w-50 rounded-full">
+            <img src="https://i.ibb.co/Btgytwv/favicon.png" />
+          </div>
+        </label>
+        <a className="btn">Login</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
