84cefed2bc576e88c0645781062dfd57e5dc6674
Rayhan Shorker <raihanshorker56@gmail.com>
2023-05-18 14:44:44 +0600

    add Auth Provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import './index.css'
 import router from './pages/router/route/router'
+import AuthProvider from './firebase/Provider/AuthProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <div className="max-w-7xl mx-auto">
     <React.StrictMode>
-    <RouterProvider router={router} />
+  <AuthProvider>
+  <RouterProvider router={router} />
+  </AuthProvider>
   </React.StrictMode>
   </div>
 )
