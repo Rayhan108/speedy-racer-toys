2584aad6ea74c688fd6a0cd977c67a4540cc7e8f
Rayhan Shorker <raihanshorker56@gmail.com>
2023-05-18 19:04:34 +0600

    get add a toy data by react hook form

--- src/pages/Home/AddToy/AddToy.jsx
+++ src/pages/Home/AddToy/AddToy.jsx
@@ -1,6 +1,9 @@
-
+import { useForm } from "react-hook-form";
 
 const AddToy = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+
     return (
         
 
@@ -10,40 +13,41 @@ const AddToy = () => {
     
     <div className="card w-full max-w-3xl shadow-2xl bg-base-100">
     <div className="card-body">
-    <form >
+    <form onSubmit={handleSubmit(onSubmit)} >
+    {errors.exampleRequired && <span>This field is required</span>}
         <h1 className="text-center font-extrabold mt-5 mb-5 text-3xl">Add A Toy</h1>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Photo URL</span>
           </label>
-          <input type="url" placeholder="Photo URL" className="input input-bordered" />
+          <input type="url" placeholder="Photo URL"  {...register("photo-url", { required: true })} className="input input-bordered" />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Toy Name</span>
           </label>
-          <input type="text" placeholder="Toy Name" className="input input-bordered" />
+          <input type="text" placeholder="Toy Name"  {...register("toy-name", { required: true })} className="input input-bordered" />
           
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Seller Name</span>
           </label>
-          <input type="text" placeholder="Seller Name" className="input input-bordered" />
+          <input type="text" placeholder="Seller Name"  {...register("seller-name", { required: true })} className="input input-bordered" />
           
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Seller Email</span>
           </label>
-          <input type="text" placeholder="Seller Email" className="input input-bordered" />
+          <input type="text" placeholder="Seller Email"  {...register("seller-email", { required: true })} className="input input-bordered" />
           
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Sub Catagory</span>
           </label>
-          <select className="input input-bordered">
+          <select className="input input-bordered" {...register("category")}>
             <option value="bike">Bike</option>
             <option value="sports-car">sports car</option>
             <option value="mini-train">Mini Train</option>
@@ -54,28 +58,28 @@ const AddToy = () => {
           <label className="label">
             <span className="label-text">Price</span>
           </label>
-          <input type="text" placeholder="Price" className="input input-bordered" />
+          <input type="text" placeholder="Price"  {...register("price", { required: true })} className="input input-bordered" />
           
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Rating</span>
           </label>
-          <input type="text" placeholder="Rating" className="input input-bordered" />
+          <input type="text" placeholder="Rating"  {...register("rating", { required: true })} className="input input-bordered" />
           
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Quantity</span>
           </label>
-          <input type="text" placeholder="Quantity" className="input input-bordered" />
+          <input type="text" placeholder="Quantity"  {...register("quantity", { required: true })} className="input input-bordered" />
           
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Description</span>
           </label>
-         <textarea   className="w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none  "></textarea>
+         <textarea {...register("description", { required: true })}  className="w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none  "></textarea>
           
         </div>
         <div className="form-control mt-6">
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,12 +1,34 @@
+
 import Banner from "../Banner/Banner";
 import Gallary from "../Gallary/GAllary";
-
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Gallary></Gallary>
+            <div className="flex font-extrabold items-center justify-center mb-10">
+            <Tabs>
+    <TabList>
+      <Tab>Bike</Tab>
+      <Tab>Sports Car</Tab>
+      <Tab>Mini Train</Tab>
+    </TabList>
+
+    <TabPanel>
+      <h2>Any content 1</h2>
+    </TabPanel>
+    <TabPanel>
+      <h2>Any content 2</h2>
+    </TabPanel>
+    <TabPanel>
+      <h2>Any content 3</h2>
+    </TabPanel>
+  </Tabs>
+            </div>
+ 
         </div>
     );
 };
