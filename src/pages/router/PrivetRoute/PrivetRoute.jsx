0e6cbac1f96d1b36d465180488aecab691f355ff
Rayhan Shorker <raihanshorker56@gmail.com>
2023-05-19 01:08:50 +0600

    notify msg to login for privet route

--- src/pages/router/PrivetRoute/PrivetRoute.jsx
+++ src/pages/router/PrivetRoute/PrivetRoute.jsx
@@ -1,13 +1,21 @@
 import { useContext } from "react";
 import { AuthContext } from "../../../Provider/AuthProvider";
 import { Navigate, useLocation } from "react-router-dom";
+import Swal from "sweetalert2";
 
 
 const PrivetRoute = ({children}) => {
     const {loader,user} =useContext(AuthContext)
     const location = useLocation();
    
-    
+    if(!user){
+        Swal.fire({
+            title: 'error!',
+            text: 'You have to log in first to go view details',
+            icon: 'error',
+            confirmButtonText: 'Cool'
+          })
+    }
         if(loader){
             return <div className='flex  justify-center mt-5 mb-5'><progress className="progress w-56"></progress></div>
         }
