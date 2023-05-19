7cd2d7307dca14ef488cc9db0e1598290322378b
Rayhan Shorker <raihanshorker56@gmail.com>
2023-05-19 16:44:02 +0600

    added my toys section in a tabular form

--- src/pages/MyToys/MyToys.jsx
+++ src/pages/MyToys/MyToys.jsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../Provider/AuthProvider";
+import MyToysTable from "./MyToysTable";
 
 
 const MyToys = () => {
@@ -10,12 +11,40 @@ useEffect(()=>{
     .then(res=>res.json())
     .then(data=>{
         setToys(data)
+        // console.log(data);
     })
-},[])
+},[user])
 
     return (
         <div>
-            <h1>my toys:{toys.length}</h1>
+            <h1 className="text-3xl font-extrabold text-center text-cyan-800 mt-5 mb-5">Toys You Added</h1>
+            <div className="overflow-x-auto w-full">
+  <table className="table w-full">
+    {/* head */}
+    <thead>
+      <tr className="text-center">
+       
+        <th>Index</th>
+        <th>Toy Info</th>
+        <th>Seller Name</th>
+        <th>Seller Email</th>
+        <th>Update</th>
+        <th>Delete</th>
+      </tr>
+    </thead>
+    <tbody>
+   {
+    toys.map((toy,i)=><MyToysTable key={toy._id} toy={toy} i={i}></MyToysTable>)
+   }
+   
+   
+     
+ 
+    </tbody>
+  
+    
+  </table>
+</div>
         </div>
     );
 };
---
+++ src/pages/MyToys/MyToysTable.jsx
@@ -0,0 +1,48 @@
+
+
+const MyToysTable = ({toy,i}) => {
+    console.log(toy);
+    const {toyName,sellerName,category,quantity,price,_id,photoUrl,sellerEmail}=toy;
+    return (
+     
+               <tr className="text-center">
+                <th>{i+1}</th> 
+       
+        <td>
+          <div className="flex items-center space-x-3">
+            <div className="avatar">
+              <div className="mask mask-squircle w-12 h-12">
+                <img src={photoUrl} alt="Avatar Tailwind CSS Component" />
+              </div>
+            </div>
+            <div>
+              <div className="font-bold">Name: {toyName}</div>
+              <div className="text-sm font-bold opacity-50">Category: {category}</div>
+              <div className="text-sm font-bold opacity-50">Price: {price}</div>
+              <div className="text-sm font-bold opacity-50">Ouantity: {quantity}</div>
+            </div>
+          </div>
+        </td>
+        <td>
+          {sellerName}
+          <br/>
+          <span className="badge badge-ghost badge-sm">Seller</span>
+        </td>
+        <td>
+          {sellerEmail}
+          <br/>
+          <span className="badge badge-ghost badge-sm">Seller</span>
+        </td>
+        
+        <th>
+          <button className="btn  btn-sm">Update</button>
+        </th>
+        <th>
+          <button className="btn  btn-sm">Delete</button>
+        </th>
+      </tr>
+       
+    );
+};
+
+export default MyToysTable;
