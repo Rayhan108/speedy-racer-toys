9278eed4e071f0b65a5ba3430e19c141aa899f1c
Rayhan Shorker <raihanshorker56@gmail.com>
2023-05-19 17:46:06 +0600

    implement search functionality on all toys page based on the toy name

--- src/pages/AllToys/AllToys.jsx
+++ src/pages/AllToys/AllToys.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import ToysTable from "./ToysTable";
 
 const AllToys = () => {
+  const [searchText,setSearchText]=useState("")
   const [toys, setToys] = useState([]);
   useEffect(() => {
     fetch("http://localhost:5000/toys")
@@ -12,6 +13,14 @@ const AllToys = () => {
       });
   }, []);
   console.log(toys);
+  const handleSearch = () => {
+    fetch(`http://localhost:5000/searchbyToy/${searchText}`)
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+        setToys(data);
+      });
+  };
   return (
     <div className="overflow-x-auto mt-5 mb-5">
       <h1 className="text-center text-3xl font-extrabold text-cyan-800 mb-5 mt-5">
@@ -20,11 +29,12 @@ const AllToys = () => {
       <div className="flex ml-5 justify-center mb-5 mt-5">
         <input
           type="text"
+          onChange={(e)=>setSearchText(e.target.value)}
           placeholder="Search"
           className="input input-bordered"
           style={{ width: "400px" }}
         />
-        <button className="btn  ml-3">Search</button>
+        <button onClick={handleSearch} className="btn  ml-3">Search</button>
       </div>
       <table className="table table-compact w-full">
         <thead className="text-center">
